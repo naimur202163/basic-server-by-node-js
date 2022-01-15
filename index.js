8bee67eeee360b98e72c38c6bb9812e95fc7f52c
unknown <you@example.com>
2022-01-15 10:43:59 +0600

    psuh

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello From The Server!');
 });
 
-server.listen(8000);
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening the requested on port 8000');
+});
